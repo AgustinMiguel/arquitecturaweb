413eeb3ce30d56147ab8e5deab977b182e14846d
Agustin Miguel <agustinmiguel96@gmail.com>
2020-09-19 15:50:04 -0300

    JPA entregable

---
+++ parcial/src/Usuario.java
@@ -0,0 +1,30 @@
+/**
+ * Esta clase representa al usuario de la aplicacion. En esta misma se registran
+ * todos los datos necesarios para el funcionamiento de la app Incluyendo una
+ * tarjeta.
+ * 
+ * @author Agustin Miguel
+ *
+ */
+
+public class Usuario {
+	private String nombre; // String que almacena el nombre del usuario
+	private String apellido; // String que almacena el apellido del usuario
+	private int dni; // Entero que almacena el dni del usuario
+	private String card; // Tarjeta
+
+/**
+ * metodo que permite al usuario pagar el servicio utilizado. El pago se debita de su tarjeta.
+ */
+	public void pay(String card) {
+		
+	}
+
+	/**
+	 * metodo que permite al usuario scanear un codigo, asi luego el sistema sabe
+	 * como resolver.
+	 */
+	public void scanCode() {
+
+	}
+}
